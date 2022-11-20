c2e623e5db03407b357eae83209d404a72b103ab
ging-dev <thanh1101dev@gmail.com>
2022-11-20 17:39:41 +0700

    feat(openapi): Generate always-up-to-date documentation

--- api/index.php
+++ api/index.php
@@ -2,6 +2,8 @@
 
 use FrameworkX\App;
 use FrameworkX\FilesystemHandler;
+use OpenApi\Generator;
+use React\Http\Message\Response;
 
 define('ROOT_DIR', dirname(__DIR__));
 
@@ -14,5 +16,6 @@ $app->get('/detail', FilterMiddleware::class, DetailController::class);
 $app->get('/read', FilterMiddleware::class, ReadController::class);
 $app->get('/resources/{path:.*}', new FilesystemHandler(ROOT_DIR.'/public'));
 $app->get('/', new FilesystemHandler(ROOT_DIR.'/public/index.html'));
+$app->get('/openapi', fn() => Response::plaintext(Generator::scan([ROOT_DIR.'/src'])->toYaml()));
 
 $app->run();
