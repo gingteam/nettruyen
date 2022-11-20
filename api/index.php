f7cd8a29d08001af382445e6c78e3745201bd0ff
ging-dev <thanh1101dev@gmail.com>
2022-11-20 17:24:21 +0700

    feat(vercel): vercel support

--- api/index.php
+++ api/index.php
@@ -3,14 +3,16 @@
 use FrameworkX\App;
 use FrameworkX\FilesystemHandler;
 
-require __DIR__.'/vendor/autoload.php';
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR.'/vendor/autoload.php';
 
 $app = new App();
 
 $app->get('/proxy', FilterMiddleware::class, ProxyController::class);
 $app->get('/detail', FilterMiddleware::class, DetailController::class);
 $app->get('/read', FilterMiddleware::class, ReadController::class);
-$app->get('/resources/{path:.*}', new FilesystemHandler(__DIR__.'/public'));
-$app->get('/', new FilesystemHandler(__DIR__.'/public/index.html'));
+$app->get('/resources/{path:.*}', new FilesystemHandler(ROOT_DIR.'/public'));
+$app->get('/', new FilesystemHandler(ROOT_DIR.'/public/index.html'));
 
 $app->run();
