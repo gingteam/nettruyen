220165bd34591d57cec78be79ae808bb0e24d1bd
ging-dev <thanh1101dev@gmail.com>
2022-11-21 16:17:17 +0700

    feat(app): remove unnecessary route

--- api/index.php
+++ api/index.php
@@ -14,7 +14,6 @@ $app = new App();
 $app->get('/proxy', FilterMiddleware::class, ProxyController::class);
 $app->get('/detail', FilterMiddleware::class, DetailController::class);
 $app->get('/read', FilterMiddleware::class, ReadController::class);
-$app->get('/resources/{path:.*}', new FilesystemHandler(ROOT_DIR.'/public'));
 $app->get('/', new FilesystemHandler(ROOT_DIR.'/public/index.html'));
 $app->get('/openapi', fn() => Response::plaintext(Generator::scan([ROOT_DIR.'/src'])->toYaml()));
 
